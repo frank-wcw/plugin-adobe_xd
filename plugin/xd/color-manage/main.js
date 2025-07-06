73c36423f2a8b35ef1f3946a387b65cca2f434a5
frank-wcw <e9ficjdev@gmail.com>
2025-07-06 16:26:05 +0800

    feat(plugin-xd-color-manage): 將拷貝功能遷移進同個插件並更名為 color-manage

--- plugin/xd/color-manage/main.js
+++ plugin/xd/color-manage/main.js
@@ -2,6 +2,7 @@
 /// https://developer.adobe.com/xd/uxp/develop/tutorials/quick-start/
 
 const assets = require('assets')
+const clipboard = require("clipboard")
 const { Color, LinearGradient, RadialGradient } = require("scenegraph")
 const uxp = require("uxp")
 const fs = uxp.storage.localFileSystem
@@ -189,7 +190,7 @@ async function exportAssetsColors () {
   })
 
   const currentDate = new Date()
-  let filename = '顏色配置_'
+  let filename = 'xd_assets_colors_'
 
   filename += currentDate.getFullYear()
   filename += (currentDate.getMonth() + 1).toString().padStart(2, '0')
@@ -208,9 +209,57 @@ async function exportAssetsColors () {
   }
 }
 
+function copyAssetsColors() {
+  const assetAllColors = assets.colors.get()
+
+  if (!assetAllColors.length) {
+    showAlert('assets 沒有任一顏色，嘗試添加顏色以嘗試複製功能')
+    return
+  }
+
+  const copyTexts = []
+  assetAllColors.forEach(e => {
+    const {
+      name,
+      color, // 若是單色才有
+      gradientType, // linear, radial 漸層才會有
+      colorStops, // 看起來是 color[]
+    } = e
+    const [, colorName] = name.match(/^([A-z]+\d+).*$/) || [undefined, '???']
+
+    if (color) {
+      copyTexts.push(`${colorName}: ${toUnoColorValue(color)}, // ${name}`)
+    } else if (gradientType) {
+      colorStops.forEach((f, i) => {
+        const { color } = f
+        copyTexts.push(`${colorName}_${i + 1}: ${toUnoColorValue(color)}, // ${name}`)
+      })
+    }
+  })
+
+  if (!copyTexts.length) {
+    showAlert('未匹配到任何可以複製的顏色！')
+    return
+  }
+
+  const colorText = sortColorNameList(copyTexts).join('\n  ')
+
+  clipboard.copyText(`{
+  ${colorText}
+}`)
+  showAlert('顏色已成功複製到剪貼簿！')
+}
+
+function toUnoColorValue (color) {
+  const hexColor = color.toHex(true)
+  if (!color.a || color.a === 255) return `'${hexColor}'`
+  return `rgba('${hexColor}', ${alphaToPercentage(color.a) / 100})`
+}
+
 module.exports = {
   commands: {
     importAssetsColors,
     exportAssetsColors,
+    copyAssetsColors,
   },
 }
