d934a845ac9e42d9f169e2896342d22ea430f971
frank-wcw <e9ficjdev@gmail.com>
2025-07-09 23:51:15 +0800

    feat(playground-vite-hono-react-ts): 接入 hono server

--- playground/vite-hono-react-ts/vite.config.ts
+++ playground/vite-hono-react-ts/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import devServer, {defaultOptions } from '@hono/vite-dev-server'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    devServer({
+      entry: './src/server.ts',
+      exclude: [
+        ...defaultOptions.exclude,
+        /.*\.svg($|\?)/,
+        /^\/(public|assets)\/.+/,
+      ],
+      injectClientScript: false,
+    }),
+  ],
 })
